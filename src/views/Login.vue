f3e877a4a8d6c0ae124817f4f27546ff9a250e63
senta-zhou <214346608+senta-zhou@users.noreply.github.com>
2026-05-15 15:44:19 +0800

    登录页面表单与布局实现

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,3 +1,110 @@
 <template>
-  <div class="login-layout">login</div>
+  <div class="container">
+    <!-- 登录标题 -->
+    <div class="title">
+      <div class="back-home">
+        <el-icon><Back /></el-icon>
+        <span>返回首页</span>
+      </div>
+      <div class="title-text">
+        <h2>登录您的账户</h2>
+        <p>请输入您的账户信息</p>
+      </div>
+    </div>
+    <!-- 登录表单 -->
+    <div class="form-container">
+      <el-form
+        ref="ruleFormRef"
+        :model="formData"
+        :rules="rules"
+        label-position="top"
+      >
+        <el-form-item label="用户名或邮箱" prop="username">
+          <el-input
+            placeholder="请输入用户名或邮箱"
+            v-model="formData.username"
+            size="large"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input
+            placeholder="请输入密码"
+            v-model="formData.password"
+            type="password"
+            show-password
+            size="large"
+          ></el-input>
+        </el-form-item>
+        <el-button
+          class="btn"
+          type="primary"
+          size="large"
+          @click="submitForm(ruleFormRef)"
+          >登录</el-button
+        >
+      </el-form>
+      <div class="footer">
+        <p>还没有账号？<router-link to="/auth/register">去注册</router-link></p>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script setup>
+  import { ref } from "vue";
+  const ruleFormRef = ref(null);
+  const formData = ref({
+    username: "",
+    password: "",
+  });
+  const rules = ref({
+    username: [
+      { required: true, message: "请输入用户名或邮箱", trigger: "blur" },
+    ],
+    password: [{ required: true, message: "请输入密码", trigger: "blur" }],
+  });
+
+  // 登录表单提交
+  const submitForm = (formEl) => {
+    if (!formEl) return;
+    // 校验表单
+    formEl.validate((valid) => {
+      if (valid) {
+        console.log("登录成功");
+      }
+    });
+  };
+</script>
+
+<style lang="scss" scoped>
+  .container {
+    width: 384px;
+    .title {
+      .back-home {
+        margin-bottom: 60px;
+      }
+      .title-text {
+        text-align: center;
+        h2 {
+          margin-bottom: 10px;
+          font-size: 36px;
+        }
+        p {
+          font-size: 18px;
+          color: #666;
+        }
+      }
+    }
+    .form-container {
+      margin-top: 30px;
+      .btn {
+        margin-top: 40px;
+        width: 100%;
+      }
+      .footer {
+        padding: 30px;
+        text-align: center;
+      }
+    }
+  }
+</style>
